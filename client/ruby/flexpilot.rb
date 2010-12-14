1bb06324378e3ad6d03aa0fad5edc5b632ceedaf
Joel Klabo <joelklabo@gmail.com>
2010-12-13 16:37:39 -0800

    added a bunch of functions to flexpilot.rb that I had only added to selenium.py until now

--- client/ruby/flexpilot.rb
+++ client/ruby/flexpilot.rb
@@ -52,6 +52,90 @@ module Selenium
       def wait_for_flex_object(locator, options)
           remote_control_command("waitForFlexObject", [locator,options])
       end
+      
+      def flex_slider_change(self, locator, options):
+          remote_control_command("flexSliderChange", [locator,options])
+      end
+
+      def flex_date_change(self, locator, options):
+          remote_control_command("flexDateChange", [locator,options])
+      end
+
+      def flex_dg_item_edit(self, locator, options):
+          remote_control_command("flexDgItemEdit", [locator,options])
+      end
+
+      def flex_dg_column_stretch(self, locator, options):
+          remote_control_command("flexDgColumnStretch", [locator,options])
+      end
+
+      def flex_dg_header_release(self, locator, options):
+          remote_control_command("flexDgHeaderRelease", [locator,options])
+      end
+
+      def flex_dg_sort(self, locator, options):
+          remote_control_command("flexDgSort", [locator,options])
+      end
+
+      def flex_dg_sort_ascending(self, locator, options):
+          remote_control_command("flexDgSortAscending", [locator,options])
+      end
+
+      def flex_dg_sort_descending(self, locator, options):
+          remote_control_command("flexDgSortDescending", [locator,options])
+      end
+
+      def flex_adg_expand_all(self, locator, options):
+          remote_control_command("flexAdgExpandAll", [locator, options])
+      end
+
+      def flex_assert_text_in_adg(self, locator, options):
+          remote_control_command("flexAssertTextInAdg", [locator, options])
+      end
+
+      def flex_assert_text_in_adg_cell(self, locator, options):
+          remote_control_command("flexAssertTextInAdgCell", [locator, options])
+      end
+
+      def flex_adg_item_open(self, locator, options):
+          remote_control_command("flexAdgItemOpen", [locator,options])
+      end
+
+      def flex_adg_item_close(self, locator, options):
+          remote_control_command("flexAdgItemClose", [locator,options])
+      end
+
+      def flex_adg_column_stretch(self, locator, options):
+          remote_control_command("flexAdgColumnStretch", [locator,options])
+      end
+
+      def flex_adg_header_release(self, locator, options):
+          remote_control_command("flexAdgHeaderRelease", [locator,options])
+      end
+
+      def flex_adg_sort(self, locator, options):
+          remote_control_command("flexAdgSort", [locator,options])
+      end
+
+      def flex_adg_sort_ascending(self, locator, options):
+          remote_control_command("flexAdgSortAscending", [locator,options])
+      end
+
+      def flex_adg_sort_descending(self, locator, options):
+          remote_control_command("flexAdgSortDescending", [locator,options])
+      end
+
+      def flex_adg_header_shift(self, locator, options):
+          remote_control_command("flexAdgHeaderShift", [locator,options])
+      end
+      
+      def flex_adg_item_edit(self, locator, options):
+          remote_control_command("flexAdgItemEdit", [locator,options])
+      end
+      
+      def flex_item_drag_drop(self, locator, options):
+          remote_control_command("flexItemDragDrop", [locator,options])
+      end
     end
   end
 end
